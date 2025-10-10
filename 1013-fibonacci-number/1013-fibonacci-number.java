73f7d3899ded1f65ea63746eccdca086f73d1405
Shubham Singh <shubham.psingh.dev@gmail.com>
2025-10-11 01:19:13 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.34%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int help(int n, int[] dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = help(n-1, dp) + help(n-2, dp);
+
+    }
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return help(n, dp);
+    }
+}
