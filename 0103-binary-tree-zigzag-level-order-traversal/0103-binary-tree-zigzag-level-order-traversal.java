a1e6d12d1d88fcc28d6dc8a9f7a6f71a086143ac
Shubham Singh <shubham.psingh.dev@gmail.com>
2025-10-22 19:31:43 +0530

    Time: 0 ms (100%), Space: 42.4 MB (40.05%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,32 +1,41 @@
 class Solution {
-    public int height(TreeNode root){
+    public int height(TreeNode root){ //Give height with respect to node... not by level
         if(root == null) return 0;
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
-    public List<Integer> levelLR(TreeNode root, int n){
-        List<Integer> list = new ArrayList<>();
-        if(root == null) return list;
-        if(n == 1) list.add(root.val);
-        else{
-            list.addAll(levelLR(root.left, n-1));
-            list.addAll(levelLR(root.right, n-1));
+    public void LR(TreeNode root, int n, List<Integer> list){
+        if(root == null) return;
+        if(n == 1){
+            list.add(root.val);
+            return;
         }
-    return list;
+        LR(root.left, n-1, list);
+        LR(root.right, n-1, list);
     }
 
+    public void RL(TreeNode root, int n, List<Integer> list){
+        if(root == null) return;
+        if(n == 1){
+            list.add(root.val);
+            return;
+        }
+        RL(root.right, n-1, list);
+        RL(root.left, n-1, list);
+    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
         int height = height(root);
-        for(int i = 1; i<=height; i++){
-            if(i % 2 != 0){
-                ans.add(levelLR(root, i));
+        for(int i = 1; i<= height; i++){
+            List<Integer> temp = new ArrayList<>();
+            if(i%2 == 1){
+                LR(root, i, temp);
             }else{
-                List<Integer> temp = new ArrayList<>(levelLR(root, i));
-                Collections.reverse(temp);
-                ans.add(temp);
+                RL(root, i, temp);
             }
+            ans.add(temp);
         }
-    return ans;
+    return ans;  
     }
 }
