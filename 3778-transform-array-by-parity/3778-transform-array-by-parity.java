ccafcf5c606f40622ded4e8d8540117881dd1183
Shubham Singh <shubham.psingh.dev@gmail.com>
2025-10-31 00:54:36 +0530

    Time: 1 ms (100%), Space: 44.9 MB (64.09%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.java
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[] transformArray(int[] nums) {
         int[] ans = new int[nums.length];
-        int i = 0;
+        int cntEven = 0;
+        int cntOdd = 0;
         for(int num : nums){
-            if(num % 2 == 0) ans[i++] = 0;
-            else ans[i++] = 1;
+            if(num % 2 == 0) cntEven++;
+            else cntOdd++;
         }
-        Arrays.sort(ans);
+        int i = 0;
+        while(cntEven-- > 0) ans[i++] = 0;
+        while(cntOdd-- > 0) ans[i++] = 1;
     return ans;
     }
 }
